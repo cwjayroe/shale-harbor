30b7dab7bd3d77c2119451375d77214e7fb1d096
Will Jayroe <jayroe.will@protonmail.com>
2021-03-23 18:08:34 -0500

    updated website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,20 @@
 import { Grid } from "@material-ui/core"
 import * as React from "react"
-import img1 from '../images/img1-scaled.jpg'
-import img2 from '../images/img2-scaled.jpg'
-import img3 from '../images/img3-scaled.jpg'
-import img4 from '../images/img4-scaled.jpg'
+import logo from '../images/logo.jpg'
+import '../styles/global.css'
 
-// styles
-const title = {
-  color: "#232129",
-  fontSize: "50px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  textAlign: "center"
-}
-
-const page = {
-  textAlign: 'center'
-}
-
-const img = {
-  maxWidth: '100%',
-  maxHeight: '1000px'
-}
-
-const titleBanner = {
-  maxHeight: '1000px',
-  minHeight: '600px',
-  marginTop: '300px'
-}
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <Grid direction='column' style={page} item>
-        <div>
-          <div style={titleBanner}>
-            <div style={{marginTop: 'auto'}}>
-              <div style={title}>
-                Shell Harbour
-              </div>
-              <div>
-                Coming Soon!
-              </div>
-              <div>
-                Have questions? Contact us at <a href="mailto:admin@shellharbourrv.com">here</a>
-              </div>
-            </div>
+    <main className='background'>
+      <Grid direction='column' className='center-grid' item>
+        <div className='center-grid__container'>
+          <div className='center-grid__container-title'>
           </div>
-          <div>
-            <img src={img1} style={img} />
-          </div>
-          <div>
-            <img src={img2} style={img} />
-          </div>
-          <div>
-            <img src={img3} style={img} />
-          </div>
-          <div>
-            <img src={img4} style={img} />
+          <div className='logo'>
+            <img src={logo} style={{ maxHeight: '250px' }} />
+            <div className='contact'>Have questions? Contact us at <a href="mailto:admin@shellharbourrv.com">here</a></div>
           </div>
         </div>
       </Grid>
